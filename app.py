2afc1a5e74b4e563843109a8b76630bb7d6c08ec
Aadil Hasan <142513544+Viper2904@users.noreply.github.com>
2025-07-23 15:51:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,9 @@ user_menu = st.sidebar.radio(
 if user_menu =='Medal Tally':
     st.sidebar.header("Medal Tally")
     years,country=helper.country_year_list(df)
+    
     selected_year=st.sidebar.selectbox("Select Year",years)
     selected_country=st.sidebar.selectbox("Select Country",country)
-    medal_tally=helper.medal_tally(df)
+    
+    medal_tally=helper.fetch_medal_tally(df,selected_year,selected_country)
     st.dataframe(medal_tally)
