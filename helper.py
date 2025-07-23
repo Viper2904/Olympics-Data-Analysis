960833dc20132ffae7d1f6aca30a2c73011aff44
Aadil Hasan <142513544+Viper2904@users.noreply.github.com>
2025-07-23 15:11:04 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -3,4 +3,9 @@ def medal_tally(df):
     medal_tally=medal_tally.groupby('region').sum()[['Gold','Silver','Bronze']].sort_values('Gold',ascending=False).reset_index()
     medal_tally['total']= medal_tally['Gold'] + medal_tally['Silver'] + medal_tally['Bronze']
     
+    medal_tally['Gold']=medal_tally['Gold'].astype('int')
+    medal_tally['Silver']=medal_tally['Silver'].astype('int')
+    medal_tally['Bronze']=medal_tally['Bronze'].astype('int')
+    medal_tally['total']=medal_tally['total'].astype('int')
+    
     return medal_tally
