3897ae94fe1b3bd819f2ea4143009a1d9c2f68f8
Aadil Hasan <142513544+Viper2904@users.noreply.github.com>
2025-07-23 15:10:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,15 @@ import streamlit as st
 import pandas as pd
 import pre_processor,helper
 
-df=pre_processor.preprocess()
+df = pd.read_csv('athlete_events.csv')
+region_df = pd.read_csv('noc_regions.csv')
+df=pre_processor.preprocess(df,region_df)
 
 user_menu = st.sidebar.radio(
     'Select an Option',
     ('Medal Tally', 'Overall Analysis', 'Country-wise Analysis', 'Athlete-wise Analysis')
 )
-st.dataframe(df)
-if user_menu=='Medal Tally':
+
+if user_menu =='Medal Tally':
     medal_tally=helper.medal_tally(df)
     st.dataframe(medal_tally)
