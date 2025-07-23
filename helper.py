8023bdac60847ec770f38655da971f14f6c13279
Aadil Hasan <142513544+Viper2904@users.noreply.github.com>
2025-07-23 15:52:08 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,5 +1,34 @@
 import numpy as np
 
+def fetch_medal_tally(year, country):
+    medal_df=df.drop_duplicates(subset=['Team','NOC','Games','Year','City','Sport','Event','Medal'])
+    flag=0
+    if year == 'Overall' and country == 'Overall':
+        temp_df = medal_df.copy()
+    if year == 'Overall' and country != 'Overall':
+        flag=1
+        temp_df = medal_df[medal_df['region'] == country].copy()
+    if year != 'Overall' and country == 'Overall':
+        temp_df = medal_df[medal_df['Year'] == int(year)].copy()
+    if year != 'Overall' and country != 'Overall':
+        temp_df = medal_df[(medal_df['Year'] == int(year)) & (medal_df['region'] == country)].copy()
+        
+    for medal in ['Gold', 'Silver', 'Bronze']:
+        if medal not in temp_df.columns:
+            temp_df.loc[:, medal] = 0
+    if flag==1:
+        x = temp_df.groupby('Year')[['Gold', 'Silver', 'Bronze']].sum().sort_values('Gold', ascending=False).reset_index()
+    else:
+        x = temp_df.groupby('region')[['Gold', 'Silver', 'Bronze']].sum().sort_values('Gold', ascending=False).reset_index()
+    
+    x['total'] = x['Gold'] + x['Silver'] + x['Bronze']
+    medal_tally['Gold']=medal_tally['Gold'].astype('int')
+    medal_tally['Silver']=medal_tally['Silver'].astype('int')
+    medal_tally['Bronze']=medal_tally['Bronze'].astype('int')
+    medal_tally['total']=medal_tally['total'].astype('int')
+    
+    return x
+
 def medal_tally(df):
     medal_tally=df.drop_duplicates(subset=['Team','NOC','Games','Year','City','Sport','Event','Medal'])
     medal_tally=medal_tally.groupby('region').sum()[['Gold','Silver','Bronze']].sort_values('Gold',ascending=False).reset_index()
