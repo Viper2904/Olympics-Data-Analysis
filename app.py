5df449f79f59cabf0864c4d5ef61ddccd8ffe4b2
Aadil Hasan <142513544+Viper2904@users.noreply.github.com>
2025-07-23 15:21:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,16 @@ df = pd.read_csv('athlete_events.csv')
 region_df = pd.read_csv('noc_regions.csv')
 df=pre_processor.preprocess(df,region_df)
 
+st.sidebar.title("Olympics Analysis")
 user_menu = st.sidebar.radio(
     'Select an Option',
     ('Medal Tally', 'Overall Analysis', 'Country-wise Analysis', 'Athlete-wise Analysis')
 )
 
 if user_menu =='Medal Tally':
+    st.sidebar.header("Medal Tally")
+    years,country=helper.country_year_list(df)
+    selected_year=st.sidebar.selectbox("Select Year",years)
+    selected_country=st.sidebar.selectbox("Select Country",country)
     medal_tally=helper.medal_tally(df)
     st.dataframe(medal_tally)
